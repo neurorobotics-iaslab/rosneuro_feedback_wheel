7e46a6d50bb4965730ebf5875c7b42f4758e2117
Luca Tonin <luca.tonin@unipd.it>
2024-07-17 13:09:45 +0200

    Changed the topic name

--- src/ControlWheel.cpp
+++ src/ControlWheel.cpp
@@ -6,7 +6,7 @@ namespace rosneuro {
 ControlWheel::ControlWheel(void) : SingleWheel("controlwheel"), p_nh_("~") {
 
 	this->pub_ = this->nh_.advertise<rosneuro_msgs::NeuroEvent>("/events/bus", 1);
-	this->sub_ = this->nh_.subscribe("/integrator/neuroprediction", 1, &ControlWheel::on_received_data, this);
+	this->sub_ = this->nh_.subscribe("/smr/neuroprediction/integrated", 1, &ControlWheel::on_received_data, this);
 
 	// Bind dynamic reconfigure callback
 	this->recfg_callback_type_ = boost::bind(&ControlWheel::on_request_reconfigure, this, _1, _2);
